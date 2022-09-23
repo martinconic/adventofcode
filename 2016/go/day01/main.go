bb70d023e473d960a0d38adebd0ac715b1957b35
Calin Martinconi <calin.martinconi@gmail.com>
2022-09-23 21:29:46 +0300

    some changes to 2016 day 1

--- 2016/go/day01/main.go
+++ 2016/go/day01/main.go
@@ -56,17 +56,74 @@ func first(data []string) {
 			}
 			dx = true
 		}
+		log.Println(dist)
 	}
 
-	log.Println(dist)
 }
 
 func second(data []string) {
+	up := true
+	dx := true
+	dist := []int{0, 0}
+	matrix := [][]int{}
+	for _, val := range data {
+		v := strings.SplitN(val, "", 2)
+		d, _ := strconv.Atoi(v[1])
+
+		if dx {
+			if v[0] == "R" {
+				if up {
+					dist[0] += d
+					up = true
+				} else {
+					dist[0] -= d
+					up = false
+				}
+			} else {
+				if up {
+					dist[0] -= d
+					up = false
+				} else {
+					dist[0] += d
+					up = true
+				}
+			}
+
+			dx = false
+		} else {
+			if v[0] == "L" {
+				if up {
+					dist[1] += d
+					up = true
+				} else {
+					dist[1] -= d
+					up = false
+				}
+			} else {
+				if up {
+					dist[1] -= d
+					up = false
+				} else {
+					dist[1] += d
+					up = true
+				}
+			}
+			dx = true
+		}
+		for _, l := range matrix {
+			if dist[0] <= l[0] && dist[1] <= l[1] {
+				log.Println(dist)
+				break
+			}
+		}
+		matrix = append(matrix, []int{dist[0], dist[1]})
+		//		log.Println(matrix)
+	}
 
 }
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("sample.txt")
 	if err != nil {
 		log.Fatalf("can not open file")
 	}
@@ -81,5 +138,6 @@ func main() {
 
 	log.Println(data)
 
-	first(data)
+	//	first(data)
+	second(data)
 }
